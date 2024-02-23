ea7dd110a86ecf27cc5fbc71bf216c7462bd920e
CarlosEduFF <Carlos.farias11@etec.sp.gov.br>
2024-02-23 09:29:23 -0300

    Mobile application proggramming lesson

---
+++ Exer6.kt
@@ -0,0 +1,5 @@
+fun main(ergs: Array<String>) {
+
+    val bomHumor = true
+    print("Hoje estou ${if (bomHumor) "feliz" else "chateado"}")
+}
---
+++ Exer9.kt
@@ -0,0 +1,11 @@
+
+
+fun main(args: Array<String>) {
+
+    val nota: Double = 6.9
+    if (nota >= 7.0) {
+        println("Aprovado!!")
+    }else {
+        println("Reprovado!!")
+    }
+}
